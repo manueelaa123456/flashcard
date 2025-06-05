a228faeda7c3ff45f39f321dbd274a2fea9990d6
manueelaa123456 <manuela.fadrique@escola.pr.gov.br>
2025-06-05 18:50:39 +0000

    aula6

---
+++ pergunta.js
@@ -0,0 +1,12 @@
+criaCartao(
+    'Filmes',
+    'Qual cena icônica do filme foi improvisada pela atriz Julia Stiles?',
+    'A cena do poema no final do filme – ela realmente chorou de verdade, sem ter sido planejado'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual ator fez sua estreia no cinema nesse filme e depois se tornou mundialmente famoso?',
+    'Heath Ledger, que interpretou Patrick Verona.s'
+)
+
