7063b63b9fbef2668bbcaca7f3cff74e0a378eac
manueelaa123456 <manuela.fadrique@escola.pr.gov.br>
2025-06-05 19:07:23 +0000

    aula 8

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Filmes',
+    'Qual cena icônica do filme foi improvisada pela atriz Julia Stiles?',
+    'A cena do poema no final do filme – ela realmente chorou de verdade, sem ter sido planejado'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual ator fez sua estreia no cinema nesse filme e depois se tornou mundialmente famoso?',
+    'Heath Ledger, que interpretou Patrick Verona.s'
+)
+
+criaCartao(
+    'Filmes',
+    'Você sabia que dois atores do filme acabaram namorando na vida real? Quem eram eles?',
+    'Julia Stiles (Kat) e Joseph Gordon-Levitt (Cameron) tiveram um breve relacionamento durante as filmagens.'
+)
+
+criaCartao(
+    'Filmes',
+    'Onde o filme foi gravado, especialmente a escola com arquitetura marcante?',
+    'No colégio Stadium High School, em Tacoma, Washington.'
+)
+
+criaCartao(
+    'Filmes',
+    'Por que o nome do filme é “10 Coisas que Eu Odeio em Você”?',
+    'Porque Kat escreve um poema com esse título, listando as coisas que "odeia" em Patrick, que na verdade mostram o quanto ela está apaixonada.'
+)
